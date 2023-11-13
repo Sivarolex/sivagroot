da8dcc3f9a478c53e6bed2c574df872570f2148c
Siva118 <sivakumardamerla@gmail.com>
2023-11-13 17:01:16 +0530

    13-11-2023-2

--- common_chars_twostrings.py
+++ common_chars_twostrings.py
@@ -9,3 +9,30 @@ string2="VANSHIKA"
 no_of_common_character=commonfun(string1.lower(),string2.lower())
  
 print("NO. OF COMMON CHRACTERS ARE : ",no_of_common_character)
+
+
+# Using Dictionary
+
+def count(str1, str2):
+    common_dict={}
+    for i in str1:
+        if i in common_dict:
+            common_dict[i]+=1
+        else:
+            common_dict[i]=1
+
+    counter = 0
+
+    for i in str2:
+        if i in common_dict and common_dict[i] > 0:
+            counter += 1
+            common_dict[i] -= 1
+    print("No. of matching characters are: " + str(counter))
+
+if __name__ == "__main__":
+    # Define two strings to compare.
+    str1 = 'aabcddekll12@'
+    str2 = 'bb2211@55k'
+ 
+    # Call the count function with the two strings.
+    count(str1, str2)
